87ff1f83aa705586fb99b0f06c56bc1e9d775797
kucheriav <kucheriav.borisovich@yandex.ru>
2025-04-20 22:37:45 +0300

    found deprecated method in main (on startup). refactored with lifespan

--- app/main.py
+++ app/main.py
@@ -1,28 +1,28 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from app.database import engine, Base
 from app.routers import user_routes, legal_entity_routes, fundraising_routes, news_routes
 
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    try:
+        async with engine.begin() as conn:
+            await conn.run_sync(Base.metadata.create_all)
+    except Exception as e:
+        print(f'DB error: {e}')
+        raise
+    yield
+    await engine.dispose()
+
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(user_routes.router, prefix="/users", tags=["users"])
 app.include_router(legal_entity_routes.router, prefix="/legal_entity", tags=["legal_entity"])
 app.include_router(fundraising_routes.router, prefix="/fundraising", tags=["fundraising"])
 app.include_router(news_routes.router, prefix="/news", tags=["news"])
 
 
-async def create_tables():
-    try:
-        async with engine.begin() as conn:
-            await conn.run_sync(Base.metadata.create_all)
-    except Exception as e:
-        print('-----')
-        print(e)
-        raise
-
-
-@app.on_event("startup")
-async def startup_event():
-    await create_tables()
 
 if __name__ == "__main__":
     import uvicorn
