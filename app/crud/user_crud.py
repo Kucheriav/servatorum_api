c1c0993f5e055d6a92eb2b14f80262fceb8ae209
kucheriav <kucheriav.borisovich@yandex.ru>
2025-04-26 14:54:59 +0300

    testing asynchro db-request fix

--- app/crud/user_crud.py
+++ app/crud/user_crud.py
@@ -1,10 +1,10 @@
+from sqlalchemy.future import select
+import logging
+from datetime import datetime
 from app.database import connection
 from app.models.user_model import User
 from app.schemas.user_schema import UserCreate
 from app.errors_custom_types import *
-import logging
-from datetime import datetime
-
 
 logger = logging.getLogger("app.user_crud")
 
@@ -14,11 +14,6 @@ class UserCRUD:
     async def create_user(self, user: UserCreate, session):
         logger.info("Creating a new user")
         try:
-            date_of_birth = (
-                datetime.strptime(user.date_of_birth, "%Y-%m-%d").date()
-                if isinstance(user.date_of_birth, str)
-                else user.date_of_birth
-            )
             new_user = User(
                 login=user.login,
                 first_name=user.first_name,
@@ -57,20 +52,28 @@ class UserCRUD:
     @connection
     async def get_user(self, user_id: int, session):
         logger.info(f"Fetching user with ID: {user_id}")
-        user = session.select(User).filter(User.id == user_id).first()
-        if user:
-            logger.info(f"User with ID {user_id} retrieved successfully")
-            return user
-        else:
-            logger.warning(f"User with ID {user_id} not found")
-            raise UserNotFoundError(f"USER_NOT_FOUND: {user_id}")
+        try:
+            query = select(User).where(User.id == user_id)
+            result = await session.execute(query)
+            user = result.scalar_one_or_none()
+            if user:
+                logger.info(f"User with ID {user_id} retrieved successfully")
+                return user
+            else:
+                logger.warning(f"User with ID {user_id} not found")
+                raise UserNotFoundError(f"USER_NOT_FOUND: {user_id}")
+        except Exception as e:
+            logger.error(f"Error occurred while fetching user with ID {user_id}", exc_info=True)
+            raise
 
     @connection
     async def patch_user(self, user_id: int, session, **params):
         logger.info(f"Patching user with ID: {user_id}")
-        user_to_patch = session.select(User).filter(User.id == user_id).first()
-        if user_to_patch:
-            try:
+        try:
+            query = select(User).where(User.id == user_id)
+            result = await session.execute(query)
+            user_to_patch = result.scalar_one_or_none()
+            if user_to_patch:
                 for key, value in params.items():
                     if hasattr(user_to_patch, key):
                         setattr(user_to_patch, key, value)
@@ -81,26 +84,28 @@ class UserCRUD:
                 await session.commit()
                 logger.info(f"User with ID {user_id} patched successfully")
                 return user_to_patch
-            except Exception as e:
-                logger.error("Error occurred while patching user", exc_info=True)
-                raise
-        else:
-            logger.warning(f"User with ID {user_id} not found")
-            raise UserNotFoundError(f"USER_NOT_FOUND: {user_id}")
+            else:
+                logger.warning(f"User with ID {user_id} not found")
+                raise UserNotFoundError(f"USER_NOT_FOUND: {user_id}")
+        except Exception as e:
+            logger.error(f"Error occurred while patching user with ID {user_id}", exc_info=True)
+            raise
 
     @connection
     async def delete_user(self, user_id: int, session):
         logger.info(f"Deleting user with ID: {user_id}")
-        user_to_delete = session.select(User).filter(User.id == user_id).first()
-        if user_to_delete:
-            try:
-                session.delete(user_to_delete)
+        try:
+            query = select(User).where(User.id == user_id)
+            result = await session.execute(query)
+            user_to_delete = result.scalar_one_or_none()
+            if user_to_delete:
+                await session.delete(user_to_delete)
                 await session.commit()
                 logger.info(f"User with ID {user_id} deleted successfully")
                 return True
-            except Exception as e:
-                logger.error("Error occurred while deleting user", exc_info=True)
-                raise
-        else:
-            logger.warning(f"User with ID {user_id} not found")
-            raise UserNotFoundError(f"USER_NOT_FOUND: {user_id}")
+            else:
+                logger.warning(f"User with ID {user_id} not found")
+                raise UserNotFoundError(f"USER_NOT_FOUND: {user_id}")
+        except Exception as e:
+            logger.error(f"Error occurred while deleting user with ID {user_id}", exc_info=True)
+            raise
