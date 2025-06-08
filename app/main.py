cace7ed5adf5cc2fb36ca205dee685e0b36bf746
Kucheriav <kucheriav.borisovich@yandex.ru>
2025-06-08 13:38:40 +0300

    local: added spheres flow

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ import logging
 from app.logging_config import setup_logging
 from app.database import engine, Base
 from app.routers import (user_routes, company_routes, foundation_routes, fundraising_routes,
-                         news_routes, wallet_routes, transaction_routes)
+                         news_routes, wallet_routes, transaction_routes, sphere_routes)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -23,13 +23,15 @@ logger = logging.getLogger(__name__)
 
 app = FastAPI(lifespan=lifespan, debug=True)
 logger.info("FastAPI starting...")
-app.include_router(user_routes.router, prefix="/users", tags=["users"])
+
 app.include_router(company_routes.router, prefix="/company", tags=["company"])
 app.include_router(foundation_routes.router, prefix="/foundation", tags=["foundation"])
 app.include_router(fundraising_routes.router, prefix="/fundraising", tags=["fundraising"])
 app.include_router(news_routes.router, prefix="/news", tags=["news"])
-app.include_router(wallet_routes.router, prefix="/wallet", tags=["wallet"])
+app.include_router(sphere_routes.router, prefix="/sphere", tags=["sphere"])
 app.include_router(transaction_routes.router, prefix="/transaction", tags=["transaction"])
+app.include_router(user_routes.router, prefix="/users", tags=["users"])
+app.include_router(wallet_routes.router, prefix="/wallet", tags=["wallet"])
 logger.info("Routers are connected")
 
 
