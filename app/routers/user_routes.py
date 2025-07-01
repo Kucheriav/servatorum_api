ccb96955dcd638009f26f017c553f299d71ae8fa
kucheriav <kucheriav.borisovich@yandex.ru>
2025-07-01 08:41:07 +0300

    troubles with bot start

--- app/routers/user_routes.py
+++ app/routers/user_routes.py
@@ -20,7 +20,7 @@ async def send_via_tg(phone: str, code: str):
     logger.info("into sending func")
     chat_id_list = await user_crud.get_chat_id_for_bot()
     for chat_id in chat_id_list:
-        bot.send_message(chat_id=chat_id, text=f'sent code {code} to {phone}')
+        await bot.send_message(chat_id=chat_id, text=f'sent code {code} to {phone}')
 
 
 def send_sms_mock(phone: str, code: str):
